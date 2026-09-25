14513c0e798b74ca1223345b576d47e06e07cc6a
Lisiee Manzoli Goncalves Pereira <lisieepereira@hotmail.com>
2026-09-24 23:28:39 -0300

    chore

--- encomendas/src/environments/environment.prod.ts
+++ encomendas/src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: (globalThis as { __APP_API_URL__?: string }).__APP_API_URL__ ?? 'http://localhost:3001',
+  apiUrl:
+    (globalThis as { __APP_API_URL__?: string }).__APP_API_URL__ ??
+    'https://encomendas-api.onrender.com',
 };
